9f350e4f13f409ba3c1e213d9378f45310e03180
bengwinter <ben.g.winter@gmail.com>
2017-03-13 20:44:31 -0400

    fixed active link

--- js/active-link.js
+++ js/active-link.js
@@ -1,4 +1,23 @@
-$(window).on('hashchange',function(){ 
-  debugger;
-    $('h1').text(location.hash.slice(1));
+
+$(function () {
+    var currentHash = "#";
+    $(document).scroll(function () {
+
+        $('.main-section').each(function () {
+            var top = window.pageYOffset + $("#navigation").height();
+            var distance = top - $(this).offset().top;
+            var hash = "#" + $(this).attr('id');
+            // 30 is an arbitrary padding choice, 
+            // if you want a precise check then use distance===0
+            if (distance < 30 && distance > -30 && currentHash != hash) {
+                $('.anchor-link').removeClass('active');
+                setTimeout(function(){ 
+                  $('a[href='+ hash +']').addClass('active');
+                }, 10);
+                
+                history.replaceState(null, null, hash);
+                currentHash = hash;
+            }
+        });
+    });
 });
