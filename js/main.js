f879cc4475dfa4b78175d84e8a26e583e63cafc9
bengwinter <ben.g.winter@gmail.com>
2017-04-02 18:12:13 -0400

    fixed hours

--- js/main.js
+++ js/main.js
@@ -28,16 +28,14 @@ var Cornwalls = {
   getDailyTimes: function() {
 
     //times go [sunday, monday, tuesday, wednesday, thursday, friday, saturday ]
-    var dailyBarTimes = ["if there is a Red Sox game or a concert at Fenway","from 12PM-2AM","from 12PM-2AM","from 12PM-2AM","from 12PM-2AM","from 12PM-2AM","from 4PM-2AM"];
-    var dailyKitchenCloseTime = ["closed unless there is a game or concert","11pm","11pm","11pm","11pm","11pm","11pm"];
+    var dailyBarTimes = ["if there is a game or concert at Fenway","from 12PM-2AM","from 12PM-2AM","from 12PM-2AM","from 12PM-2AM","from 12PM-2AM","from 4PM-2AM"];
+    var dailyKitchenCloseTime = ["11PM (if there is a Fenway event)","11PM","11PM","11PM","11PM","11PM","11PM"];
 
     //do not add 0 for month
     var specialDateTimes = {"1-01-2017": {"kitchen": "11PM", "bar": "4am-2am"}};
     
     var today = new Date();
     var specialDateCheck = (today.getMonth() + 1) + '-' + today.getDate() + "-" + today.getFullYear();
-
-
     
     if (specialDateTimes[specialDateCheck] === undefined) {
       $('.daily-time').text(dailyBarTimes[today.getDay()]);
